4c896cc16a46d6c63910556d67b463a5ed99ef72
CF Garden <garden@pivotal.io>
2017-06-15 16:05:32 +0300

    Revert "Add new e2fs and quota-tools progs to the stemcell"
    This reverts commit ab85ad5eb8dfe157bc800ce9876d895c82be4a72.

--- bin/repack-stemcell/repack.sh
+++ bin/repack-stemcell/repack.sh
@@ -8,13 +8,9 @@ stemcell_tgz=/tmp/stemcell.tgz
 stemcell_dir=/tmp/stemcell
 image_dir=/tmp/image
 
-agent_dir=${PWD}
-
 mkdir -p $stemcell_dir $image_dir
 wget -O- $STEMCELL_URL > $stemcell_tgz
 echo "$STEMCELL_SHA1  $stemcell_tgz" | shasum -c -
-rm -rf /tmp/build/hello
-mkdir -p /tmp/build/hello
 
 # Expose loopbacks in concourse container
 (
@@ -45,41 +41,10 @@ mkdir -p /tmp/build/hello
 		cd $image_dir
 		tar xvf $stemcell_dir/image
 		mnt_dir=/mnt/stemcell
-		mkdir -p $mnt_dir
-		umount $mnt_dir || true
+		mkdir $mnt_dir
 		mount -o loop,offset=32256 root.img $mnt_dir
 		echo -n 0.0.${new_ver} > $mnt_dir/var/vcap/bosh/etc/stemcell_version
-		cp ${agent_dir}/bin/bosh-agent $mnt_dir/var/vcap/bosh/bin/bosh-agent
-
-		cd $mnt_dir/tmp
-		# wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.8.17/linux-headers-4.8.17-040817_4.8.17-040817.201701090438_all.deb
-		# wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.8.17/linux-headers-4.8.17-040817-generic_4.8.17-040817.201701090438_amd64.deb
-		# wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.8.17/linux-image-4.8.17-040817-generic_4.8.17-040817.201701090438_amd64.deb
-
-
-		cd $mnt_dir
-		mount -t proc proc proc/
-		chmod -R 777 tmp
-
-		sudo chroot $mnt_dir /bin/bash <<EOF
-		  mkdir -p /tmp/user/0
-		  echo "nameserver 8.8.8.8" > /etc/resolv.conf
-		  apt update
-		  apt install git autoconf autopoint -y
-		  cd /tmp
-		  wget https://www.kernel.org/pub/linux/kernel/people/tytso/e2fsprogs/v1.43.4/e2fsprogs-1.43.4.tar.gz
-		  tar xzf e2fsprogs-1.43.4.tar.gz
-		  cd e2fsprogs-1.43.4
-		  ./configure
-		  make
-		  make install
-
-		  cd ..
-		  git clone git://git.kernel.org/pub/scm/utils/quota/quota-tools.git
-		  cd quota-tools
-		  ./autogen.sh && ./configure && make
-		  make
-EOF
+		cp /tmp/build/*/agent-src/bin/bosh-agent $mnt_dir/var/vcap/bosh/bin/bosh-agent
 
 		if [ -n "$BOSH_DEBUG_PUB_KEY" ]; then
 			sudo chroot $mnt_dir /bin/bash <<EOF
@@ -91,15 +56,13 @@ EOF
 				chown -R bosh_debug:bosh_debug .
 EOF
     fi
-    		umount $mnt_dir/proc
-		cd $image_dir
+
 		umount $mnt_dir
-		tar czf $stemcell_dir/image *
+		tar czvf $stemcell_dir/image *
 	)
 
 	sed -i.bak "s/version: .*/version: 0.0.${new_ver}/" stemcell.MF
 	tar czvf $stemcell_tgz *
 )
 
-mkdir -p /tmp/build/hello/stemcell
-cp $stemcell_tgz /tmp/build/hello/stemcell/
+cp $stemcell_tgz /tmp/build/*/stemcell/
