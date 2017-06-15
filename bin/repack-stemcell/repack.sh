2b6ce0d7543ca83633a3464636f5b1b58e6d4997
Georgi Sabev <georgethebeatle@gmail.com>
2017-06-15 17:28:11 +0300

    Update the repack script to update kernel and run in concourse
    Signed-off-by: Tiago Scolari <tscolari@pivotal.io>

--- bin/repack-stemcell/repack.sh
+++ bin/repack-stemcell/repack.sh
@@ -48,12 +48,59 @@ echo "$STEMCELL_SHA1  $stemcell_tgz" | shasum -c -
 
 		if [ -n "$BOSH_DEBUG_PUB_KEY" ]; then
 			sudo chroot $mnt_dir /bin/bash <<EOF
+			        groupadd bosh_sudoers
+				groupadd bosh_sshers
 				useradd -m -s /bin/bash bosh_debug -G bosh_sudoers,bosh_sshers
-				cd ~bosh_debug
+				cd ~/bosh_debug
 				mkdir .ssh
 				echo $BOSH_DEBUG_PUB_KEY >> .ssh/authorized_keys
 				chmod go-rwx -R .
 				chown -R bosh_debug:bosh_debug .
+EOF
+
+			 sudo chroot $mnt_dir /bin/bash <<EOF
+			   mkdir -p /tmp/user/0
+			   echo "nameserver 8.8.8.8" > /etc/resolv.conf
+			   apt update
+			   apt install git autoconf autopoint -y
+			   cd /tmp
+			   wget https://www.kernel.org/pub/linux/kernel/people/tytso/e2fsprogs/v1.43.4/e2fsprogs-1.43.4.tar.gz
+			   tar xzf e2fsprogs-1.43.4.tar.gz
+			   cd e2fsprogs-1.43.4
+			   ./configure
+			   make
+			   make install
+
+			   cd ..
+			   git clone git://git.kernel.org/pub/scm/utils/quota/quota-tools.git
+			   cd quota-tools
+			   ./autogen.sh && ./configure && make
+			   make
+
+			   extended_version=4.6.7-040607_4.6.7-040607.201608160432
+			   version=$(echo ${extended_version} | sed "s/-generic//")
+			   specific_version=$(echo ${extended_version} | cut -d _ -f1)
+			   short_version="v$(echo ${specific_version} | cut -d - -f1)"
+
+			   mkdir -p /var/vcap/data/kernel-${version}
+
+			   add-apt-repository -y ppa:ubuntu-toolchain-r/test
+			   apt-get update
+			   apt-get install -y gcc-6 g++-6
+			   unlink /usr/bin/gcc
+			   ln -s /usr/bin/gcc-6 /usr/bin/gcc
+
+			   sed -i "s/error UTS_UBUNTU_RELEASE_ABI/warning UTS_UBUNTU_RELEASE_ABI/" /usr/src/ixgbevf-3.3.2/src/kcompat.h
+
+			   pushd /var/vcap/data/kernel-${version}
+			     wget http://kernel.ubuntu.com/~kernel-ppa/mainline/${short_version}/linux-headers-${version}_all.deb
+			     wget http://kernel.ubuntu.com/~kernel-ppa/mainline/${short_version}/linux-headers-${extended_version}_amd64.deb
+			     wget http://kernel.ubuntu.com/~kernel-ppa/mainline/${short_version}/linux-image-${extended_version}_amd64.deb
+
+			     dpkg -i *.deb
+			   popd
+
+			   sed -i "s/$(uname -r)/${specific_version}/" /boot/grub/menu.lst
 EOF
     fi
 
